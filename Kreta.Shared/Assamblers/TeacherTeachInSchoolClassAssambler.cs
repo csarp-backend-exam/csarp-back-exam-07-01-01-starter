dbcd7cd78dd8316071788829883a77f7a16d3a72
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-04-10 15:50:53 +0200

    Shared

---
+++ Kreta.Shared/Assamblers/TeacherTeachInSchoolClassAssambler.cs
@@ -0,0 +1,24 @@
+﻿using Kreta.Shared.Dtos;
+using Kreta.Shared.Extensions;
+using Kreta.Shared.Models.SwitchTable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kreta.Shared.Assamblers
+{
+    public class TeacherTeachInSchoolClassAssambler : Assambler<TeacherTeachInSchoolClass, TeacherTeachInSchoolClassDto>
+    {
+        public override TeacherTeachInSchoolClassDto ToDto(TeacherTeachInSchoolClass domainEntity)
+        {
+            return domainEntity.ToDto();
+        }
+
+        public override TeacherTeachInSchoolClass ToModel(TeacherTeachInSchoolClassDto dto)
+        {
+            return dto.ToModel();
+        }
+    }
+}
---
+++ Kreta.Shared/Dtos/TeacherDto.cs
@@ -0,0 +1,21 @@
+﻿using Kreta.Shared.Models;
+
+namespace Kreta.Shared.Dtos
+{
+    public class TeacherDto
+    {
+        public Guid Id { get; set; } 
+        public string FirstName { get; set; } =string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime BirthDay { get; set; }
+        public string PlaceOfBirth { get; set; } = string.Empty;
+        public bool IsWoman { get; set; }
+        public bool IsHeadTeacher { get; set; }
+        public Guid HeadTeacherForShoolClassId { get; set; }
+        public virtual SchoolClass? HeadTeacherFoClass { get; set; }
+        public string MathersName { get; set; } = string.Empty;
+        public Guid? AddressId { get; set; }
+        public virtual Address? Address { get; set; }
+
+    }
+}

ff0edfd06ffe4983890a2d79ee716f6273815e04
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-04-10 15:20:48 +0200

    A feladat kódja

---
+++ Kreta.Backend/Repos/Managers/IRepositoryManager.cs
@@ -0,0 +1,11 @@
+﻿using Kreta.Backend.Repos.SwitchTables;
+
+namespace Kreta.Backend.Repos.Managers
+{
+    public interface IRepositoryManager
+    {
+        public ISchoolClassRepo? SchoolClassRepo { get; }
+        public ISubjectRepo? SubjectRepo { get; }
+        public ISchoolClassSubjectsRepo? SchoolClassSubjectsRepo { get; }
+    }
+}
